d58fb3b92492321a22f39b57efe67fa0ba41fcb6
rycue <151157051+rycue@users.noreply.github.com>
2024-10-31 13:15:19 +0800

    Version 3: Update file1.py

--- file1.py
+++ file1.py
@@ -1,10 +1,13 @@
 import random
 
-def foo():
-    """This function does something"""
+def foo() -> Optional[int]:
+    """This function returns a random integer between 0 and 10, or None if a randomly chosen boolean is False."""
     x = random.randint(0, 10)
+    # randomly choose a boolean
     y = random.choice([True, False])
     if y:
+        # return the integer
         return x
     else:
+        # return None
         return None
